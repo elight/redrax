b5a53580130b0bcf771799908ce381880d0caa7c
Evan Light <evan@tripledogdare.net>
2014-07-18 12:20:02 -0400

    * Extract method on the wrong-case finder * Docs

--- lib/redrax/metadata_marshaller.rb
+++ lib/redrax/metadata_marshaller.rb
@@ -1,4 +1,11 @@
 module Redrax
+  # Converts input into a format we can use to update Metadata against the API
+  # @param [Hash] hash The hash of metadata we're going to marshal
+  # @param [String] prefix The value we want to have prefix the key-value pairs
+  # @param [Hash] options Accepts:
+  #   * wrong: `String` or `Array` of `String`s of incorrect prefixes the caller
+  #     may have supplied.  We can then identify and correct these for the
+  #     caller.
   class MetadataMarshaller
     def call(hash, prefix, options = {})
       hash.each_with_object({}) { |(key, v), h|
@@ -8,13 +15,21 @@ module Redrax
         new_key = 
           if key =~ /^#{prefix.downcase}/
             key
-          elsif options[:wrong] && found = Array(options[:wrong]).find { |w| key =~ /^#{w.downcase}/ }
+          elsif found = downcase_find(options[:wrong], key)
             key.gsub(/#{found}/, prefix)
           else
             prefix + key
           end
         h[new_key] = v
       }
+    end
+
+    private
+
+    def downcase_find(src, term)
+      if src 
+        Array(src).find { |w| term =~ /^#{w.downcase}/ }
+      end
     end
   end
 end
