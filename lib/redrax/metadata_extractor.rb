fdc0d37af6a53fe8b6a160f3865cfc9d3f815552
Evan Light <evan@tripledogdare.net>
2014-08-04 14:11:09 -0400

    Another oopsy (thanks @smashwilson)

--- lib/redrax/metadata_extractor.rb
+++ lib/redrax/metadata_extractor.rb
@@ -2,7 +2,7 @@ module Redrax
   class MetadataExtractor
     def call(pattern, headers)
       headers.each_with_object({}) { |header, h|
-        (k, v) = header
+        k, v = header
         if k =~ /#{pattern}-?/i
           name = k.gsub(/^#{pattern}-?/i, "")
           h[name] = v
