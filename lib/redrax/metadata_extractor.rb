277f627a08c4c66b91c484ec7342848de6b6a8da
Evan Light <evan@tripledogdare.net>
2014-08-12 11:30:13 -0400

    Cleaned up YARDoc

--- lib/redrax/metadata_extractor.rb
+++ lib/redrax/metadata_extractor.rb
@@ -1,5 +1,12 @@
 module Redrax
   class MetadataExtractor
+    # Extracts and returns (only) Metadata from HTTP response headers
+    # @param [String] pattern The prefix of the names of the Metadata fields 
+    #  in the HTTP response headers, e.g., x-object-meta for Cloud Files objects
+    # @param [Hash] headers The HTTP headers provided by the HTTP response
+    # @return [Hash] the Metadata extracted from the HTTP response.  The keys
+    #   will *not* contain the `pattern` prefix, e.g., "x-object-meta-foo"
+    #   would simply have the key "foo".
     def call(pattern, headers)
       headers.each_with_object({}) { |(k, v), h|
         if k =~ /#{pattern}-?/i
--- lib/redrax/metadata_marshaller.rb
+++ lib/redrax/metadata_marshaller.rb
@@ -1,12 +1,12 @@
 module Redrax
-  # Converts input into a format we can use to update Metadata against the API
-  # @param [Hash] hash The hash of metadata we're going to marshal
-  # @param [String] prefix The value we want to have prefix the key-value pairs
-  # @param [Hash] options Accepts:
-  #   * wrong: `String` or `Array` of `String`s of incorrect prefixes the caller
-  #     may have supplied.  We can then identify and correct these for the
-  #     caller.
   class MetadataMarshaller
+    # Converts input into a format we can use to update Metadata against the API
+    # @param [Hash] hash The hash of metadata we're going to marshal
+    # @param [String] prefix The value we want to have prefix the key-value pairs
+    # @param [Hash] options Accepts:
+    #   * wrong: `String` or `Array` of `String`s of incorrect prefixes the caller
+    #     may have supplied.  We can then identify and correct these for the
+    #     caller.
     def call(hash, prefix, options = {})
       hash.each_with_object({}) { |(key, v), h|
         key = key.to_s.downcase
